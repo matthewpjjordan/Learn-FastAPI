a1ffacf710ff7bd945b16158c214ed7deffc7ece
matthewpjjordan <mattj549@hotmail.com>
2023-05-03 20:10:11 +0200

    Add first POST method

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from typing import Optional
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -6,6 +9,17 @@ app = FastAPI()
 def root():
     return {"message": "That's better"}
 
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
 @app.get("/posts")
 def get_posts():
     return {"data": "These are your posts"}
+
+@app.post("/createposts")
+def create_posts(post: Post):
+    print(post.dict())
+    return {"data": post}
