f31c8c06e24ffa9f4d3bcfa81d1ed871faa11598
matthewpjjordan <matthewphilipjordan@gmail.com>
2023-05-04 10:05:55 +0200

    Add in memory storage of added posts

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
+# uvicorn main:app --reload
+
 from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
-
-@app.get("/")
-def root():
-    return {"message": "That's better"}
 
 class Post(BaseModel):
     title: str
@@ -15,11 +14,20 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1},
+            {"title": "fav food", "content": "pizza number 1", "id": 2}]
+
+@app.get("/")
+def root():
+    return {"message": "That's better"}
+
 @app.get("/posts")
 def get_posts():
-    return {"data": "These are your posts"}
+    return {"data": my_posts}
 
-@app.post("/createposts")
+@app.post("/posts")
 def create_posts(post: Post):
-    print(post.dict())
-    return {"data": post}
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 1000000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
