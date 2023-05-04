bda313e41012b9fed93d86c05e9be5b68ab69999
matthewpjjordan <matthewphilipjordan@gmail.com>
2023-05-04 14:53:57 +0200

    Add DELETE method for /posts path parameter

--- main.py
+++ main.py
@@ -22,6 +22,11 @@ def find_post(id):
         if post["id"] == id:
             return post
 
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
+
 @app.get("/")
 def root():
     return {"message": "That's better"}
@@ -44,3 +49,10 @@ def get_post(id: int):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f"post with id {id} was not found.")
     return {"post_detail": post}
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_post(id)
+    
+    my_posts.pop(index)
+    return {'message': 'post was succesfully deleted'}
