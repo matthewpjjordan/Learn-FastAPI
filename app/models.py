3e52521404b71ad1f26a815b59be72f45aa0f57d
matthewpjjordan <matthewphilipjordan@gmail.com>
2023-05-07 13:31:13 +0200

    Add create_at column to posts table

--- app/models.py
+++ app/models.py
@@ -1,11 +1,18 @@
 from .database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, Time
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
 
 
+# sqlalchemy only performs these actions if the table does not exist.
+# It will not detect differences in setup and update the database.
 class Post(Base):
     __tablename__ = "posts"
 
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    published = Column(Boolean, default=True)
+    published = Column(Boolean, server_default="True", nullable=False)
+    created_at = Column(
+        TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
+    )
