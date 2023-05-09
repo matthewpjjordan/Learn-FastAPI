476113f99340816c18d9783cc02b4e7e8650fb99
matthewpjjordan <matthewphilipjordan@gmail.com>
2023-05-09 09:12:03 +0200

    Add pydantic models for api responses

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 
 class PostBase(BaseModel):
@@ -13,3 +14,13 @@ class PostCreate(PostBase):
 
 class PostDelete(PostBase):
     pass
+
+
+class Post(PostBase):
+    id: int
+    created_at: datetime
+
+    # Pydantic works with dicts. Data returned by ORM query is a sqlalchemy model.
+    # Below has Pydantic read data even if its not a dict, but an ORM model.
+    class Config:
+        orm_mode = True
