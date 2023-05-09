94684d5cf8a867bebbc5c49af573019b087d7406
matthewpjjordan <matthewphilipjordan@gmail.com>
2023-05-09 08:14:06 +0200

    Change get, post, update method to return cleaner data

--- app/main.py
+++ app/main.py
@@ -48,7 +48,7 @@ def get_posts(db: Session = Depends(get_db)):
     # cursor.execute("""SELECT * FROM posts""")
     # posts = cursor.fetchall()
     posts = db.query(models.Post).all()
-    return {"data": posts}
+    return posts
 
 
 @app.post(
@@ -69,7 +69,7 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     # refresh contents of instance from db e.g. includes defaults created in db
     db.refresh(new_post)
 
-    return {"data": new_post}
+    return new_post
 
 
 @app.get("/posts/{id}")
@@ -82,7 +82,7 @@ def get_post(id: int, db: Session = Depends(get_db)):
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"post with id {id} was not found.",
         )
-    return {"post_detail": post}
+    return post
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
@@ -125,4 +125,4 @@ def update_post(
     post_query.update(updated_post.dict(), synchronize_session=False)
     db.commit()
 
-    return {"data": post_query.first()}
+    return post_query.first()
