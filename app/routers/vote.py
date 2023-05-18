59ebe105c5f0a10c5627a0d3e2522c9f75472fb1
matthewpjjordan <matthewphilipjordan@gmail.com>
2023-05-18 17:09:18 +0200

    Add post exist test to vote route

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -16,6 +16,13 @@ def vote(
     db: Session = Depends(get_db),
     current_user: schemas.UserOut = Depends(oauth2.get_current_user),
 ):
+    post = db.query(models.Post).filter(models.Post.id == vote.post_id).first()
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            details=f"Post with id: {vote.post_id} does not exist",
+        )
+
     vote_query = db.query(models.Vote).filter(
         models.Vote.post_id == vote.post_id, models.Vote.user_id == current_user.id
     )
