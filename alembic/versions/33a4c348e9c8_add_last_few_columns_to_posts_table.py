3bd54cbdfa337d27e3d39590ccb42115da54ec09
matthewpjjordan <matthewphilipjordan@gmail.com>
2023-05-19 18:21:43 +0200

    add columns to post table

---
+++ alembic/versions/33a4c348e9c8_add_last_few_columns_to_posts_table.py
@@ -0,0 +1,24 @@
+"""add last few columns to posts table
+
+Revision ID: 33a4c348e9c8
+Revises: 153b980cc60c
+Create Date: 2023-05-19 16:04:57.148606
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '33a4c348e9c8'
+down_revision = '153b980cc60c'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
