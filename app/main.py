709db96d1371e0876f13dd6604d0c5a3dd5d62cd
matthewpjjordan <matthewphilipjordan@gmail.com>
2023-05-21 17:57:32 +0200

    Add cors middlewear

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from . import models
 from .routers import post, user, auth, vote
 from .database import engine
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 
@@ -12,6 +13,19 @@ load_dotenv()
 # models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # e.g. GET, POST etc
+    allow_headers=["*"],
+)
 
 # Request will enter post file and check each route in turn.
 app.include_router(post.router)
