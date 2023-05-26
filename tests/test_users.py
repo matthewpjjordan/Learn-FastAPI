43eddb825219eede4969d0b8446acd9bedc829af
matthewpjjordan <matthewphilipjordan@gmail.com>
2023-05-26 09:47:41 +0200

    Add login user test

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,5 +1,17 @@
+import pytest
 from app import schemas
 from .database import client, session
+
+
+@pytest.fixture()
+def create_test_user(client):
+    user_data = {"email": "hello3@gmail.com", "password": "supersafestring"}
+    res = client.post("/users/", json=user_data)
+
+    assert res.status_code == 201
+    new_user = res.json()
+    new_user["password"] = user_data["password"]
+    return new_user
 
 
 def test_root(client):
@@ -16,3 +28,14 @@ def test_create_user(client):
     new_user = schemas.UserOut(**res.json())
     assert res.status_code == 201
     assert new_user.email == "hello3@gmail.com"
+
+
+def test_login_user(client, create_test_user):
+    res = client.post(
+        "/login",
+        data={
+            "username": create_test_user["email"],
+            "password": create_test_user["password"],
+        },
+    )
+    assert res.status_code == 200
